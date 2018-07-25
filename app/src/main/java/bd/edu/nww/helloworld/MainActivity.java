053e4cbe368372c6d08fb157063c3506b85cfabb
Faisal Ahmed <faisal.ahmedbcse@gmail.com>
2018-07-25 19:55:39 +0600

    Signup button added

--- app/src/main/java/bd/edu/nww/helloworld/MainActivity.java
+++ app/src/main/java/bd/edu/nww/helloworld/MainActivity.java
@@ -10,10 +10,10 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     String userName;
     TextView tvShowName;
-    Button btn_login;
+    Button btn_login, btnSignup;
     EditText edtUserName;
 
     @Override
@@ -23,17 +23,33 @@ public class MainActivity extends AppCompatActivity {
         edtUserName = findViewById(R.id.edt_userName);
         btn_login = findViewById(R.id.btn_login);
         tvShowName = findViewById(R.id.textView_showName);
+        btn_login.setOnClickListener(this);
+        btnSignup = findViewById(R.id.btn_signup);
+        btnSignup.setOnClickListener(this);
+
+        //findViewById(R.id.btn_signup).setOnClickListener(this);
 
 
-        btn_login.setOnClickListener(new View.OnClickListener() {
+       /* btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 userName = edtUserName.getText().toString();
                 tvShowName.setText(userName);
                 Toast.makeText(MainActivity.this, userName, Toast.LENGTH_SHORT).show();
             }
-        });
+        });*/
 
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.btn_login) {
+            userName = edtUserName.getText().toString();
+            tvShowName.setText(userName);
+            Toast.makeText(MainActivity.this, userName, Toast.LENGTH_SHORT).show();
+        } else if (view.getId() == R.id.btn_signup) {
+            Toast.makeText(MainActivity.this, "Sing up button pressed", Toast.LENGTH_SHORT).show();
+        }
     }
 }
